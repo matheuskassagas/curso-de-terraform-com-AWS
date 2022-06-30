14f2c830199e59d7a0c43f30de00a0a02a0234f1
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-06-30 14:14:09 -0300

    Data-source-ec2-data-outputs-created

--- 05-data-source/ec2/data.tf
+++ 05-data-source/ec2/data.tf
@@ -1,3 +1,5 @@
 data "aws_ami" "ubuntu" {
-
+  owners      = ["amazon"]
+  most_recent = true
+  name_regex  = "ubuntu"
 }
--- 05-data-source/ec2/ec2.tf
+++ 05-data-source/ec2/ec2.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "web" {
-  ami           = ""
+  ami           = data.aws_ami.ubuntu
   instance_type = var.instance_type
 }
