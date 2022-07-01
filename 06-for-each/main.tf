6f42c2f8b836b526053408f1badc0381c6323fcc
Matheus Gigliotti <gigliotti_@hotmail.com>
2022-07-01 16:29:36 -0300

    for-each

---
+++ 06-for-each/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "1.2.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
